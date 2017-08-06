12f85ff6c817aca8f81d0810e176e4f352b247af
Octavian Ionescu <itavyg@gmail.com>
2017-08-07 01:00:03 +0300

    added setup for e2e testing
    Signed-off-by: Octavian Ionescu <itavyg@gmail.com>

---
+++ test/e2e/Fixtures.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const rabbitMQURI = 'amqp://pinomqusr:pinomqpwd@localhost/pino-mq';
+const rabbitMQExchange = 'pinoMQExchange';
+
+module.exports = {
+  rabbitMQURI,
+  rabbitMQExchange,
+};
