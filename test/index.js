5c63620e3b9bc8d093307ec6aab1f1e555f4dde9
Octavian Ionescu <itavyg@gmail.com>
2017-08-06 23:37:51 +0300

    added module export tests
    Signed-off-by: Octavian Ionescu <itavyg@gmail.com>

--- test/index.js
+++ test/index.js
@@ -4,4 +4,5 @@ describe('Pino MQ', () => {
   require('./Helpers'); // eslint-disable-line global-require
   require('./RabbitMQ'); // eslint-disable-line global-require
   require('./MQTransport'); // eslint-disable-line global-require
+  require('./ModuleExport'); // eslint-disable-line global-require
 });
