4b03d865042e46e3236442b758e3724044b1302b
Octavian Ionescu <itavyg@gmail.com>
2017-08-16 23:14:23 +0300

    updated pino-mq exec
    Signed-off-by: Octavian Ionescu <itavyg@gmail.com>

--- pino-mq.js
+++ pino-mq.js
@@ -12,25 +12,26 @@ const pkgInfo = require('./package.json');
 
 const defaultOptions = {
   type:         'RABBITMQ',
-  uri:          '',
+  uri:          null,
   exchange:     '',
   queue:        null,
   queuePattern: null,
   queueMap:     null,
-  fields:       [],
+  fields:       null,
   config:       null,
 };
 
 const longOptions = {
-  type:         ['RABBITMQ'],
-  uri:          String,
-  exchange:     String,
-  queue:        String,
-  queuePattern: String,
-  fields:       String,
-  config:       String,
-  help:         Boolean,
-  version:      Boolean,
+  type:           ['RABBITMQ'],
+  uri:            String,
+  exchange:       String,
+  queue:          String,
+  queuePattern:   String,
+  fields:         String,
+  config:         String,
+  help:           Boolean,
+  version:        Boolean,
+  generateConfig: Boolean,
 };
 
 const shortOptions = {
@@ -43,6 +44,7 @@ const shortOptions = {
   c:  '--config',
   h:  '--help',
   v:  '--version',
+  g:  '--generateConfig',
 };
 
 const argv = nopt(longOptions, shortOptions, process.argv);
@@ -58,28 +60,86 @@ if (configOptions.help) {
   process.exit(0);
 }
 
+if (configOptions.generateConfig) {
+  const cfgSample = JSON.stringify({
+    type:         'RABBITMQ',
+    uri:          'amqp://guest:guest@localhost/',
+    exchange:     '',
+    queue:        'pino-mq',
+    queuePattern: null,
+    queueMap:     null,
+    fields:       [],
+  }, null, ' ');
+  fs.writeFileSync('pino-mq.json', cfgSample);
+  console.log('Configuration is written in file "pino-mq.json"');
+  console.log('You can use now:');
+  console.log('\n\nnode script.js | pino-mq -c pino-mq.json\n\n');
+  process.exit(0);
+}
+
+if (configOptions.fields && (configOptions.fields.length !== 0)) {
+  configOptions.fields = configOptions.fields.split(',');
+}
+
+if (configOptions.config !== null) {
+  try {
+    // eslint-disable-next-line import/no-dynamic-require, global-require
+    const cfgFile = require(path.resolve(configOptions.config));
+    Object.keys(configOptions).map((key) => {
+      if (configOptions[key]) {
+        return null;
+      }
+      if (cfgFile[key]) {
+        configOptions[key] = cfgFile[key];
+      }
+      return null;
+    });
+  } catch (e) {
+    console.log(`Error loading config file: ${e.message}`);
+    process.exit(1);
+  }
+}
+
+if (configOptions.uri === null) {
+  console.log('You must specify connection uri');
+  process.exit(1);
+}
+
+
+// process.exit(0);
+
 // eslint-disable-next-line import/no-dynamic-require
 const getMqTransport = require(path.join(__dirname, 'index')).getTransport;
 
 
 const t = getMqTransport({
-  type:            'RABBITMQ',
+  type:            configOptions.type,
   transportParams: {
-    uri: '',
+    uri: configOptions.uri,
   },
-  exchange: '',
-  queue:    'test-queue',
-  // queuePattern,
-  // queueMap,
-  fields:   [],
+  exchange:     configOptions.exchange,
+  queue:        configOptions.queue,
+  queuePattern: configOptions.queuePattern,
+  queueMap:     configOptions.queueMap,
+  fields:       configOptions.fields,
 });
 
-process.stdin.on('close', t.close.bind(t));
-process.on('SIGINT', t.close.bind(t));
-process.on('SIGTERM', t.close.bind(t));
+process.stdin.on('close', () => {
+  console.log('STDIN CLOSE');
+  t.close();
+});
+// process.on('SIGINT', t.close.bind(t));
+process.on('SIGINT', () => {
+  console.log('SIGINT CLOSE');
+  t.close();
+});
+process.on('SIGTERM', () => {
+  console.log('SIGTERM CLOSE');
+  t.close();
+});
 
 pump(
-  t,
+  process.stdin,
   split(JSON.parse),
-  through.obj(t.write.bind(t), t.close.bind(t)));
-
+  through.obj(t.write.bind(t)));
+// through.obj(t.write.bind(t), t.close.bind(t)));
